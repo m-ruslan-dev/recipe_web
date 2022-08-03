5e491896bf990c41de3d8fd6cb55b3ad737f5197
rus-77 <casp.ricci@gmail.com>
2022-08-03 23:41:07 +0300

    Add useNavigate to searchbar

--- src/components/Searchbar/Searchbar.js
+++ src/components/Searchbar/Searchbar.js
@@ -1,10 +1,19 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import { FiSearch } from "react-icons/fi";
 
 const Searchbar = () => {
+    const navigate = useNavigate();
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        let query = event.target.query.value;
+        navigate(`/search?s=${query}`);
+    }
+
     return (
-        <form action="/search" method="get" className="searchbar">
-            <input className="searchbar__input" type="input" name="s" placeholder="Find recipe"/>
+        <form action="#" method="get" className="searchbar" onSubmit={(e) => handleSubmit(e)}>
+            <input className="searchbar__input" type="input" name="query" placeholder="Find recipe"/>
             <button type="submit" className="searchbar__submit"><FiSearch /></button>
         </form>        
     )
